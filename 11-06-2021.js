f754e3662c67a251bf6c42edff74674655500fe6
katiemdev <katiem498@gmail.com>
2021-06-11 19:51:05 +0100

    code wars

---
+++ 11-06-2021.js
@@ -0,0 +1,4 @@
+//Implement String#six_bit_number?, which should return true if given object is a number representable by 6 bit unsigned integer (0-63), false otherwise. It should only accept numbers in canonical representation, so no leading +, extra 0s, spaces etc.
+String.prototype.sixBitNumber = function () {
+	return /^([0-9]|[1-5][0-9]|6[0-3])$/.test(this);
+};
