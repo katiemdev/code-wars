fa150d2ca997dae78892854c51af514ddb80898b
katiemdev <katiem498@gmail.com>
2021-06-13 00:00:59 +0100

    code wars

---
+++ 13-06-2021.js
@@ -0,0 +1,7 @@
+//Create a function that takes any sentence and redistributes the spaces (and adds additional spaces if needed) so that each word starts with a vowel. The letters should all be in the same order but every vowel in the sentence should be the start of a new word. The first word in the new sentence may start without a vowel. It should return a string in all lowercase with no punctuation (only alphanumeric characters).
+let vowelStart = (str) =>
+	str
+		.toLowerCase()
+		.replace(/[^a-z0-9]/gi, "")
+		.replace(/[aeiou]/gi, (m) => ` ${m}`)
+		.trim();
