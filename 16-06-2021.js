f5bc64bf976a76fad291db5096bdbdbdb9f4a3ca
katiemdev <katiem498@gmail.com>
2021-06-16 22:43:45 +0100

    code wars

---
+++ 16-06-2021.js
@@ -0,0 +1,4 @@
+//Implement the function which should return true if given object is a vowel (meaning a, e, i, o, u), and false otherwise.
+String.prototype.vowel = function () {
+	return /^[aeiou]{1}$/i.test(this);
+};
