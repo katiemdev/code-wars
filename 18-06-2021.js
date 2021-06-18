cd68e6cb65daf5832d5bc79c2a47c697c7142ec9
katiemdev <katiem498@gmail.com>
2021-06-18 12:03:34 +0100

    code wars

--- 18-06-2021.js
+++ 18-06-2021.js
@@ -11,3 +11,12 @@ function firstNonRepeated(s) {
 	}
 	return null;
 }
+
+//Mash 2 arrays together so that the returning array has alternating elements of the 2 arrays . Both arrays will always be the same length.
+function arrayMash(array1, array2) {
+	let answer = [];
+	for (let i = 0; i < array1.length; i++) {
+		answer.push(array1[i], array2[i]);
+	}
+	return answer;
+}
