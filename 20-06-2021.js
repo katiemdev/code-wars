d1dfc82d6f8543101c7bbca7c03cf40ef9ff22c0
katiemdev <katiem498@gmail.com>
2021-06-20 22:06:45 +0100

    code wars

--- 20-06-2021.js
+++ 20-06-2021.js
@@ -14,3 +14,10 @@ function reverseWords(str) {
 		.map((w) => (w = w.split("").reverse().join("")))
 		.join(" ");
 }
+
+//Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+String.prototype.toJadenCase = function () {
+	return this.split(" ")
+		.map((w) => (w = w[0].toUpperCase() + w.slice(1).toLowerCase()))
+		.join(" ");
+};
