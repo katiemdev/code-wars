9937678a97aa00ecef14dfa57e1d43e0d3832d66
katiemdev <katiem498@gmail.com>
2021-06-21 15:25:01 +0100

    code wars

--- 21-06-2021.js
+++ 21-06-2021.js
@@ -6,3 +6,20 @@ function bucketOf(str) {
 	if (/i don't know|slime/gi.test(str)) return "slime";
 	return "air";
 }
+
+//One suggestion to build a satisfactory password is to start with a memorable phrase or sentence and make a password by extracting the first letter of each word. Even better is to replace some of those letters with numbers (e.g., the letter O can be replaced with the number 0):
+// instead of including i or I put the number 1 in the password;
+// instead of including o or O put the number 0 in the password;
+// instead of including s or S put the number 5 in the password.
+function makePassword(phrase) {
+	let password = [];
+	phrase = phrase.split(" ");
+	for (let i = 0; i < phrase.length; i++) {
+		password.push(phrase[i].slice(0, 1));
+	}
+	return password
+		.join("")
+		.replace(/i/gi, "1")
+		.replace(/o/gi, "0")
+		.replace(/s/gi, "5");
+}
