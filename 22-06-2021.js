262f549c89c4549446dcf1bb94f8662ce5c271c6
katiemdev <katiem498@gmail.com>
2021-06-22 21:52:21 +0100

    code wars

--- 22-06-2021.js
+++ 22-06-2021.js
@@ -37,3 +37,7 @@ function gematria(str) {
 	}
 	return total;
 }
+
+//When given a string of space separated words, return the word with the longest length. If there are multiple words with the longest length, return the last instance of the word with the longest length.
+const longestWord = (str) =>
+	str.split(" ").reduce((a, c) => (c.length >= a.length ? (a = c) : a), "");
