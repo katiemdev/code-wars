f92c55c225bb440d3ca0f734348b3095a0d3ba15
katiemdev <katiem498@gmail.com>
2021-06-23 23:22:52 +0100

    code wars

--- 23-06-2021.js
+++ 23-06-2021.js
@@ -8,3 +8,13 @@ function removeRotten(bag) {
 		  )
 		: [];
 }
+
+//Given a string S. You have to return another string such that even-indexed and odd-indexed characters of S are grouped and groups are space-separated (see sample below)
+function sortMyString(S) {
+	let odd = "",
+		even = "";
+	for (let l in S) {
+		l % 2 === 0 ? (even += S[l]) : (odd += S[l]);
+	}
+	return `${even} ${odd}`;
+}
