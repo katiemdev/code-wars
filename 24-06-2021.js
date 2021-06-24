41d221e1e0a78f64c152cc2e0e66bd7ea877b658
katiemdev <katiem498@gmail.com>
2021-06-24 20:32:29 +0100

    code wars

--- 24-06-2021.js
+++ 24-06-2021.js
@@ -1,2 +1,10 @@
 //Your mission is to write a function which removes this noise from the message. Notice that noise can only be %$&/#·@|º\ª charaters, other characteres are not considered noise.
 const removeNoise = (str) => str.replace(/[%$&\/#·@|º\\ª]/gi, "");
+
+//// reverse the letters in the sentence
+function reverser(sentence) {
+	return sentence
+		.split(" ")
+		.map((word) => (word = word.split("").reverse().join("")))
+		.join(" ");
+}
