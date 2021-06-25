03a45b2156437950fc258ba2ac924afa1fa73022
katiemdev <katiem498@gmail.com>
2021-06-25 22:30:27 +0100

    code wars

--- 25-06-2021.js
+++ 25-06-2021.js
@@ -11,3 +11,22 @@ function password(str) {
 		str.length >= 8
 	);
 }
+
+//Create a function that takes an input String and returns a String, where all the uppercase words of the input String are in front and all the lowercase words at the end. The order of the uppercase and lowercase words should be the order in which they occur. If a word starts with a number or special character, skip the word and leave it out of the result.
+function capitalsFirst(str) {
+	str = str.split(" ");
+	const upper = [];
+	const lower = [];
+
+	for (let i in str) {
+		if (str[i].match(/^[^a-z]/i)) {
+			continue;
+		} else if (str[i][0] === str[i][0].toUpperCase()) {
+			upper.push(str[i]);
+		} else {
+			lower.push(str[i]);
+		}
+	}
+
+	return [...upper, ...lower].join(" ");
+}
