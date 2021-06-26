39febfa0c2f4b5a3274537e0cd4472c69591ca53
katiemdev <katiem498@gmail.com>
2021-06-26 20:22:21 +0100

    code wars

--- 26-06-2021.js
+++ 26-06-2021.js
@@ -5,3 +5,22 @@ function stringMerge(string1, string2, letter) {
 		string2.slice(string2.indexOf(letter))
 	);
 }
+
+//You are given an initial 2-value array (x). You will use this to calculate a score. If both values in (x) are numbers, the score is the sum of the two. If only one is a number, the score is that number. If neither is a number, return 'Void!'. Once you have your score, you must return an array of arrays. Each sub array will be the same as (x) and the number of sub arrays should be equal to the score.
+function explode(x) {
+	let score;
+	if (x.every((e) => typeof e === "number")) {
+		score = x.reduce((p, c) => p + c, 0);
+	} else if (x.some((e) => typeof e === "number")) {
+		score = x.filter((e) => typeof e === "number")[0];
+	} else {
+		return "Void!";
+	}
+
+	const arr = [];
+	for (let i = 1; i <= score; i++) {
+		arr.push(x);
+	}
+
+	return arr;
+}
