729c91ec3569beb3441741269e19a1414752ce6c
katiemdev <katiem498@gmail.com>
2021-06-27 21:04:06 +0100

    code wars

--- 27-06-2021.js
+++ 27-06-2021.js
@@ -8,3 +8,6 @@ function alphaSeq(str) {
 		.map((e) => (e = e.toUpperCase() + e.repeat(alphabet.indexOf(e))))
 		.join(",");
 }
+
+//Check that a number is a valid number that has been given to 2 decimal places. The number passed to the function will be given as a string. If the number satisfies the criteria below, the function should return true, else it should return false.
+const validNumber = (num) => /^[+-]?\d*\.\d{2}$/.test(num);
