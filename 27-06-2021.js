bfefdbbc923af7176cb107cc6b8323629e0c89b6
katiemdev <katiem498@gmail.com>
2021-06-27 21:09:59 +0100

    code wars

--- 27-06-2021.js
+++ 27-06-2021.js
@@ -11,3 +11,19 @@ function alphaSeq(str) {
 
 //Check that a number is a valid number that has been given to 2 decimal places. The number passed to the function will be given as a string. If the number satisfies the criteria below, the function should return true, else it should return false.
 const validNumber = (num) => /^[+-]?\d*\.\d{2}$/.test(num);
+
+//Some people just have a first name; some people have first and last names and some people have first, middle and last names. You task is to initialize the middle names (if there is any).
+function initializeNames(name) {
+	name = name.split(" ");
+	let newName = [];
+
+	if (name.length > 2) {
+		newName = name.map(
+			(e, i) =>
+				(e = i != 0 && i != name.length - 1 ? e[0].toUpperCase() + "." : e)
+		);
+		return newName.join(" ");
+	} else {
+		return name.join(" ");
+	}
+}
