3930d328955333ffc6c112e4e182f74560f7e152
katiemdev <katiem498@gmail.com>
2021-07-02 21:10:51 +0100

    code wars

---
+++ 02-07-2021.js
@@ -0,0 +1,2 @@
+//You received an array with two strings. Create a function that will return their product as a string. E.g.
+const arrMultiply = (arr) => (parseInt(arr[0]) * parseInt(arr[1])).toString();
