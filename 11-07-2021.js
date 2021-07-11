4761455396a733ba7aba7cb8a04a527b86851a8c
katiemdev <katiem498@gmail.com>
2021-07-11 21:32:32 +0100

    code wars

--- 11-07-2021.js
+++ 11-07-2021.js
@@ -5,3 +5,9 @@ function sum(digits) {
 	const sum = digits.reduce((p, c) => p + +c, 0);
 	return `${digits.join(" + ").toString()} = ${sum}`;
 }
+
+//Write a function, nicknameGenerator that takes a string name as an argument and returns the first 3 or 4 letters as a nickname. If the 3rd letter is a consonant, return the first 3 letters. If the 3rd letter is a vowel, return the first 4 letters. If the string is less than 4 characters, return "Error: Name too short".
+function nicknameGenerator(name) {
+	if (name.length < 4) return "Error: Name too short";
+	return !name[2].match(/[aeiou]/i) ? name.slice(0, 3) : name.slice(0, 4);
+}
