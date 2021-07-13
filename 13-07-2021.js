9f7fa2329af245c2b73e2bed279bb0c9446459e6
katiemdev <katiem498@gmail.com>
2021-07-13 22:50:48 +0100

    code wars

---
+++ 13-07-2021.js
@@ -0,0 +1,5 @@
+//Given a string, you must decide whether or not it contains a valid phone number. If it does, return the corrected phone number as a string ie. '02078834982' with no whitespace or special characters, else return "Not a phone number".
+function isItANum(str) {
+	str = str.replace(/\D/gi, "");
+	return /^0\d{10}$/.test(str) ? str : "Not a phone number";
+}
