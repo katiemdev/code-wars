e37cfd9af1b54443baf6be6e0dd8821abefdcd3f
katiemdev <katiem498@gmail.com>
2021-07-15 22:55:48 +0100

    code wars

---
+++ 15-07-2021.js
@@ -0,0 +1,5 @@
+//Write a method that will search an array of strings for all strings that contain another string, ignoring capitalization. Then return an array of the found strings. The method takes two parameters, the query string and the array of strings to search, and returns an array. If the string isn't contained in any of the strings in the array, the method returns an array containing a single string: "Empty"
+function wordSearch(query, seq) {
+	const answer = seq.filter((e) => new RegExp(query, "gi").test(e));
+	return answer.length > 0 ? answer : ["Empty"];
+}
