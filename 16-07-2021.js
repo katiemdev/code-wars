09a47d9456466cd7166d90efd10d8ef1b9f839e9
katiemdev <katiem498@gmail.com>
2021-07-16 19:57:39 +0100

    code wars

---
+++ 16-07-2021.js
@@ -0,0 +1,4 @@
+//your job is to write a function that accepts a string and a number as arguments.
+function repeater(string, n) {
+	return `"${string}" repeated ${n} times is: "${string.repeat(n)}"`;
+}
