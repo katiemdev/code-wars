68865fa967dcdddf717ef0cec5bf295ca9ec8afb
katiemdev <katiem498@gmail.com>
2021-07-20 21:49:13 +0100

    code wars

--- 20-07-2021.js
+++ 20-07-2021.js
@@ -7,3 +7,11 @@ const stringCounter = (inputS, charS) => {
 	const char = new RegExp(`${charS}`, "g");
 	return (inputS.match(char) || []).length;
 };
+
+//Write a function that takes a string and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's. All non-vowels including non alpha characters (spaces,commas etc.) should be included
+function vowelOne(s) {
+	return s
+		.split("")
+		.map((e) => (e = /[aeiou]/i.test(e) ? "1" : "0"))
+		.join("");
+}
