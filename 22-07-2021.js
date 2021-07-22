7155f9a92a000730781f9cf8fea61cfbd49a9964
katiemdev <katiem498@gmail.com>
2021-07-22 20:43:15 +0100

    code wars

---
+++ 22-07-2021.js
@@ -0,0 +1,7 @@
+//In this easy kata your function has to take a string as input and return a string with everything removed (whitespaces included) but the digits. As you may have guessed empty strings are to be returned as they are & if the input string contains no digits then the output will be an empty string.By the way , you have to watch out for non-string inputs too.Return 'Invalid input !' for them.
+function digitAll(x) {
+	if (typeof x !== "string") return "Invalid input !";
+	if (x == "") return x;
+
+	return x.replace(/\D/gi, "");
+}
