c4872a8ce64f6c042165302ebc4cc161ceca644f
katiemdev <katiem498@gmail.com>
2021-08-10 21:22:31 +0100

    code wars

--- 10-08-2021.js
+++ 10-08-2021.js
@@ -13,3 +13,31 @@ function wordsToMarks(string) {
 
 	return count;
 }
+
+//You are given an n by n ( square ) grid of characters, for example:
+// [['m', 'y', 'e'],
+// ['x', 'a', 'm'],
+// ['p', 'l', 'e']]
+// You are also given a list of integers as input, for example:
+// [1, 3, 5, 8]
+// You have to find the characters in these indexes of the grid if you think of the indexes as:
+// [[1, 2, 3],
+// [4, 5, 6],
+// [7, 8, 9]]
+// Remember that the indexes start from one and not zero.
+// Then you output a string like this:
+// "meal"
+function gridIndex(grid, indices) {
+	let arr = ["0"];
+
+	for (let i in grid) {
+		arr.push(...grid[i]);
+	}
+
+	let str = "";
+	for (let i in indices) {
+		str += arr[indices[i]];
+	}
+
+	return str;
+}
