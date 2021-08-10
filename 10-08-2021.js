fc4cab3797a5500cb4f940c4e8218d9ad147a740
katiemdev <katiem498@gmail.com>
2021-08-10 21:25:58 +0100

    code wars

--- 10-08-2021.js
+++ 10-08-2021.js
@@ -41,3 +41,10 @@ function gridIndex(grid, indices) {
 
 	return str;
 }
+
+//The function must return the truncated version of the given string up to the given limit followed by "..." if the result is shorter than the original. Return the same string if nothing was truncated.
+function solution(string, limit) {
+	if (limit >= string.length) return string;
+
+	return string.slice(0, limit) + "...";
+}
