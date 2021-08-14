9a0329af4507e3072d53a38066d5053c433c1435
katiemdev <katiem498@gmail.com>
2021-08-14 19:41:41 +0100

    code wars

---
+++ 14-08-2021.js
@@ -0,0 +1,13 @@
+//My PC got infected by a strange virus. It only infects my text files and replaces random letters by *, li*e th*s (like this). Fortunately, I discovered that the virus hides my censored letters inside root directory. It will be very tedious to recover all these files manually, so your goal is to implement uncensor function that does the hard work automatically.
+function uncensor(infected, discovered) {
+	infected = infected.split("");
+	let index = 0;
+
+	for (let i in infected) {
+		if (infected[i] == "*") {
+			infected[i] = discovered[index];
+			index++;
+		}
+	}
+	return infected.join("");
+}
